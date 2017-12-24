837b4e48931f0c92c0efa523cf8ba4602f08c996
Tayyab <tayyabrasheed330@gmail.com>
2017-12-24 17:41:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/tee/sharedpreferences/SharedPreferenceActivity.java
@@ -0,0 +1,43 @@
+package com.example.tee.sharedpreferences;
+
+import android.annotation.SuppressLint;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SharedPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_preference);
+
+                // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new PrefsFragment()).commit();
+
+            }
+
+            public static class PrefsFragment extends PreferenceFragment {
+
+                @SuppressLint("ResourceType")
+                @Override
+                public void onCreate(Bundle savedInstanceState) {
+                    super.onCreate(savedInstanceState);
+
+                    // Load the preferences from an XML resource
+                    addPreferencesFromResource(R.layout.activity_shared_preference);
+                }
+            }
+
+
+
+
+
+
+
+    }
+}
